0530e19b970936a69a31ec5be222a11d36d609ea
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-09-27 14:00:52 +0200

    Préparation deploy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ROLLAND_GRELLETY_Carole_12_sportsee_062025/",
   plugins: [react()],
 })
